17b84f71ccd7162eda527bb57a8023583cdae20a
Will Grime <will.grimier@gmail.com>
2024-08-19 02:13:06 +1000

    wrote tests to check the password checker

--- cypress/e2e/ValidSignup.cy.js
+++ cypress/e2e/ValidSignup.cy.js
@@ -1,5 +1,5 @@
 /// <reference types ="cypress"/>
-it('Check Valid signup on first page', () => {
+it('Check Valid signup on first page with enter', () => {
     cy.visit("business-signup")
     // I realise randomly adding a new email each time this test is run probably isn't best practice, should probably be accompanied
     // by backend method to remove @example.com emails
@@ -11,4 +11,21 @@ it('Check Valid signup on first page', () => {
     cy.get('[data-testid="ds-letter-casing-feedback"] > .sc-kBRoID > [data-testid="ds-alert-brand-check-icon ds-check-circle-icon"] > path')
     cy.get('.sc-RYuTI').click()
     cy.get('[data-testid="email-sign-up"]').click()
+    cy.get('[data-testid="registration-email-subtext-container"]').should('not.exist')
+})
+
+it('Check Valid signup on first page with clicking button', () => {
+    cy.visit("business-signup")
+    // I realise randomly adding a new email each time this test is run probably isn't best practice, should probably be accompanied
+    // by backend method to remove @example.com emails
+    cy.get('[data-testid="registration-email"]').type('hello' + Math.random().toString() + '@example.com')
+    cy.get('[data-testid="submit-button"]').click()
+    cy.get('[data-testid="registration-password"]').type('Hell0Wor!d')
+    cy.get('[data-testid="ds-minimum-length-feedback"] > .sc-kBRoID > [data-testid="ds-alert-brand-check-icon ds-check-circle-icon"] > path')
+    cy.get('[data-testid="ds-numeric-feedback"] > .sc-kBRoID > [data-testid="ds-alert-brand-check-icon ds-check-circle-icon"] > path')
+    cy.get('[data-testid="ds-symbol-feedback"] > .sc-kBRoID > [data-testid="ds-alert-brand-check-icon ds-check-circle-icon"] > path')
+    cy.get('[data-testid="ds-letter-casing-feedback"] > .sc-kBRoID > [data-testid="ds-alert-brand-check-icon ds-check-circle-icon"] > path')
+    cy.get('.sc-RYuTI').click()
+    cy.get('[data-testid="email-sign-up"]').click()
+    cy.get('[data-testid="registration-email-subtext-container"]').should('not.exist')
 })
